1ffeada6e6279292f2507d7b0813860c6d04d330
Tesfa <tesfat64@gmail.com>
2021-04-29 11:48:18 +0200

    I have added more plots to the analysisbs file. More updates to come later

--- project_files/beautiful_soup/analysisbs.py
+++ project_files/beautiful_soup/analysisbs.py
@@ -1,70 +1,28 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
-# import numpy
 
+# load the data from the csv file. 
+data = pd.read_csv("~/Desktop/Stock_Price_Scraping/output_fixtures/beautiful_soup/stocks_bs.csv") 
 
-data = pd.read_csv("~/Desktop/Stock_Price_Scraping/stocks_bs.csv") 
-# print(data.head())
+# select numeric variables for plotting
+data = data.iloc[0:5,] # select the top 5 rows for readability. 
+data1 = data["Previous Close"].to_list() # change the dataframe columns to lists
+data2 = data["Open"].to_list()
+data3 = data["PE Ratio (TTM)"].to_list()
+data4 = data["EPS (TTM)"].to_list()
+plt.close("all")
 
-# fig = plt.figure()
-# ax = fig.add_axes([0,0,1,1])
+year=data["Symbols"].to_list()
 
-
-# data.iloc[:,4].plot.bar(); 
-
-
-# fig = plt.figure()
-# ax = fig.add_axes([0,0,1,1])
-# ax.bar(data[::1],data[::4])
-# plt.show(block=True)
-
-
-# data1 = data.sort_values("Price")
-# # f = plt.figure(1)
-# data1.plot(x ='Symbols', y=['Price'], kind = 'bar')
-# plt.show(block=True) 
-
-
-# data2 = data.sort_values("Change")
-# # g = plt.figure(2)
-# data2.plot(x ='Symbols', y=['Change'], kind = 'bar')
-# plt.show(block=True) 
-
-
-
-
-# data3 = data.sort_values("Price")
-
-# data3.plot(x ='Symbols', y=['Price','Change'], kind = 'bar')
-# plt.show(block=True) 
-# data1 = data.sort_values("Price")
-# data2 = data.sort_values("Change")
-# fig, axs = plt.subplots(2)
-# fig.suptitle('Stock Exchange Analysis')
-# axs[0].plot(x ='Symbols', y=['Price'], kind = 'bar')
-# axs[1].plot(x ='Symbols', y=['Change'], kind = 'bar')
-# plt.show(block=True) 
-
-
-# fig, (ax1, ax2) = plt.subplots(2, sharex=True)
-# fig.suptitle('Aligning x-axis using sharex')
-# ax1.plot(data1.iloc[:,1], data1.iloc[:,3], kind = 'bar')
-# ax2.plot(data2.iloc[:,1], data1.iloc[:,4], kind = 'bar')
-# plt.show(block=True) 
-
-# dat = data[['Price','Change']]
-# axes = dat.plot(kind='bar',rot=0,lw=2,colormap='jet',
-#              title='Stock Exchange Analysis', subplots=True, layout=(2,1))
-
-# plt.show() 
-
-
-data1 = data.sort_values("Previous Close")
-data2 = data.sort_values("Open")
-
-ax1 = plt.subplot(311)
-data1.plot(x ='Symbols', y=['Previous Close'], kind = 'bar')
-plt.show(block=True)
-data2.plot(x ='Symbols', y=['Open'], kind = 'bar')
-plt.show(block=True)
+fig,ax=plt.subplots(4,1,figsize=(10,8)) # the title on top
+fig.suptitle('Stock market analysis', fontsize=16)
+ax[0].bar(year,data1,color="red")
+ax[0].legend(["Previous Close"])
+ax[1].bar(year,data2,color="yellow")
+ax[1].legend(["Open"])
+ax[2].bar(year,data3,color="green")
+ax[2].legend(["PE Ratio (TTM)"])
+ax[3].bar(year,data4,color="green")
+ax[3].legend(["EPS (TTM)"])
+plt.show()
